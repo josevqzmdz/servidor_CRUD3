579f55b2260f0dced001444a7118b4250ea2165f
josevqzmdz <38343804+josevqzmdz@users.noreply.github.com>
2019-12-10 21:05:37 -0600

    se logro conseguir los valores desplegados del datagridview

--- servidor_CRUD3/eliminar_usuario.cs
+++ servidor_CRUD3/eliminar_usuario.cs
@@ -42,7 +42,7 @@ namespace servidor_CRUD3
 
             // comando para seleccionar todas las celdas de una fila cuando esta se seleccione
             // https://stackoverflow.com/questions/13672693/how-do-i-select-a-complete-datagridview-row-when-the-user-clicks-a-cell-of-that
-            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            // dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
 
             try
             {
@@ -86,30 +86,60 @@ namespace servidor_CRUD3
             // boilerplate sacado de https://docs.microsoft.com/en-us/dotnet/framework/winforms/controls/selected-cells-rows-and-columns-datagridview
             // TODO: todo
             // https://www.c-sharpcorner.com/UploadFile/1e050f/insert-update-and-delete-record-in-datagridview-C-Sharp/
+            // https://www.c-sharpcorner.com/UploadFile/9582c9/insert-update-delete-display-data-in-mysql-using-C-Sharp/
 
             MySqlConnection conexion = new MySqlConnection("datasource=127.0.0.1; port=3306; username=root; password=; database = usuarios;");
             conexion.Open();
 
+            // https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.datagridview?view=netframework-4.8
             try
             {
-                // consigue el numero de celdas seleccionadas
-                Int32 celdas_seleccionadas = dataGridView1.GetCellCount(DataGridViewElementStates.Selected);
-                if (celdas_seleccionadas > 0)
+                if(this.dataGridView1.SelectedRows.Count > 0 && 
+                    this.dataGridView1.SelectedRows[0].Index != 
+                    this.dataGridView1.Rows.Count - 1)
                 {
-                    // para detectar si todas las celdas de datagrid estan seleccionadas
-                    if (dataGridView1.AreAllCellsSelected(true))
+                    // https://social.msdn.microsoft.com/Forums/en-US/7d1cf6ff-144b-4968-934f-3c5170cb03fa/how-to-get-the-selected-row-value-in-datagridview-control?forum=csharpgeneral
+                    string nombre = this.dataGridView1.SelectedCells[0].Value.ToString();
+                    string ID = this.dataGridView1.SelectedCells[1].Value.ToString();
+                    string contraseña = this.dataGridView1.SelectedCells[2].Value.ToString();
+
+                    // para los comandos DELETE de mysql
+                    // https://www.tutorialspoint.com/mysql/mysql-delete-query.htm
+                    string sqlQuery = "DELETE FROM usuario WHERE nombre = @nombre AND ID = @ID AND @contrasena = @contrasena;";
+
+                    //crear un objeto mysqlparameter para cada parametro a añadir al query
+                    /*https://www.dreamincode.net/forums/topic/268104-the-right-way-to-query-a-database-parameterizing-your-sql-queries/
+                     * SqlCommand has a collection of parameters, appropriately named Parameters. 
+                     * This has a very handy method on it named "AddWithValue". It takes a string 
+                     * and an object. First, the name of the parameter, and next the value to be 
+                     * set to that parameter.
+                     */
+                    MySqlCommand cmd = new MySqlCommand(sqlQuery, conexion);
+                    cmd.Parameters.AddWithValue("@nombre", nombre);
+                    cmd.Parameters.AddWithValue("@contrasena", contrasena);
+                    cmd.Parameters.AddWithValue("@ID", ID);
+
+                    // ahora si, ejecuta el cursor
+                    // Sends the CommandText to the Connection and builds a SqlDataReader.
+                    // https://docs.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlcommand.executereader?view=netframework-4.8
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    // TODO: read regresa falso. porque?
+                    // si pasa la conexion
+                    if (reader.Read())
                     {
-                        MessageBox.Show("todas las celdas estan seleccionadas");
+                        Console.WriteLine("usuario " + nombre + " ID: " + ID + " eliminado exitosamente");
+                        // https://docs.microsoft.com/en-us/dotnet/api/system.windows.forms.datagridview?view=netframework-4.8
+                        this.dataGridView1.Rows.RemoveAt(this.dataGridView1.SelectedRows[0].Index);
                     }
                     else
                     {
-
-                        Console.WriteLine(nombre);
-                        Console.WriteLine(ID);
-                        Console.WriteLine(contrasena);
-                        
-                        
+                        Console.WriteLine("conexion fallida");
                     }
+                }
+                else
+                {
+                    Console.WriteLine("conexion fallida");
                 }
             }
             catch (Exception ex)
